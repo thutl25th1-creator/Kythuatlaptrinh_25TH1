eda2dccadb747ffd3dda641ed1eed5fe918da94a
PMT201 <PMT201@PMT3045>
2026-03-06 10:11:33 +0700

    buoi1

---
+++ Kythuatlaptrinh/buoi1/buoi1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int a = 5;
+	int* p = &a;
+	a += 6;
+	a = *p + 2;
+	cout << *p;
+
+	/*vector <float>
+		diemthi;
+	diemthi.push_back(5.5);
+	diemthi.push_back(4);
+	diemthi.push_back(6);
+	diemthi.pop_back();
+	for (int i = 0; i < diemthi.size(); i++) {
+		cout << diemthi[i] << endl;
+	}*/
+}
