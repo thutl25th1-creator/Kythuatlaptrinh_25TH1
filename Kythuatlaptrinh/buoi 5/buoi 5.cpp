4c28beb530d509b90c6ad5fcbc734480ea017fed
Administrator <Administrator@PMT2045>
2026-04-06 14:01:43 +0700

    bai tap tt

--- Kythuatlaptrinh/buoi 5/buoi 5.cpp
+++ Kythuatlaptrinh/buoi 5/buoi 5.cpp
@@ -65,6 +65,29 @@ struct LinkedList {
         p->next = head;
         head = p;
     }
+    bool Remove(int removeId) {
+        if (head == NULL) {
+            cout << "No book available" << endl;
+            return true;
+        }
+        Node* item = head;
+        if (item->data.id == removeId) {//Xoa dau danh sach
+            head = item->next;
+            delete item;
+            return true;
+        }
+
+        while (item->next != NULL) {
+            if (item->next->data.id == removeId) {
+                Node* temp = item->next;
+                item->next = item->next->next;
+                delete temp;
+                return true;
+            }
+            item = item->next;
+        }
+        return false;
+    }
 };
 
 
@@ -101,6 +124,14 @@ int main()
             break;
         }
         case 3: {
+            int removeId;
+            cout << "Enter book's id to remove: ";
+            cin >> removeId;
+            bool res = books.Remove(removeId);
+            if (res)
+                cout << "Remove book successfully" << endl;
+            else
+                cout << "Invalid book id" << endl;
             break;
         }
         case 4: {
